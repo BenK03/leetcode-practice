47ac0e019a6c344eb246b0c669461e4b962159c3
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2026-01-12 20:19:16 -0500

    completed leetcode 283 move zeros

---
+++ dsa/array/283_move_zeros.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+
+        # move all non-zeroes to the front
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+
+        # time complexity O(n)
+        
